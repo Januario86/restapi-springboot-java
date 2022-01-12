e2facf50efbbb485d44cde4682ba6c8f02de2a42
Januario86 <januario86fp@gmail.com>
2022-01-12 19:36:22 -0300

    feat:2.7. Criando um controller com Spring MVC

--- src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
+++ src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MeuPrimeiroController {
 	
-	@GetMapping("/hello")
+	@GetMapping("/helloapi")
 	@ResponseBody
 	public String hello() {
 		
